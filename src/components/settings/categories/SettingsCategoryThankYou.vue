7614000c9a977445290689176668913de60c3b88
PlayXman <play.man.life@seznam.cz>
2021-06-26 19:34:10 +1000

    Public Release - Rename the app - Refactor package archive generation task - Add .env.local template - Rename app widget components - Add links to repo and issues to settings - Public release stuff:   - Add LICENSE   - Add issue templates   - Add Code of Conduct   - Add pull request template   - Update README texts

--- src/components/settings/categories/SettingsCategoryThankYou.vue
+++ src/components/settings/categories/SettingsCategoryThankYou.vue
@@ -1,18 +1,26 @@
 <template>
   <SettingsSection class="thankYou">
+    <h5>Project and issues</h5>
+    <p>
+      All the source files can be found on
+      <SimpleLink href="https://github.com/PlayXman/app-my-spotlight" external
+        >Github</SimpleLink
+      >. Any ideas for new features or bugs file in the
+      <SimpleLink
+        href="https://github.com/PlayXman/app-my-spotlight/issues"
+        external
+        >Issues on Github</SimpleLink
+      >.
+    </p>
     <h5>Powered by:</h5>
     <p>
-      <a href="https://unsplash.com/" target="_blank" rel="noopener">
-        Unsplash
-      </a>
+      <SimpleLink href="https://unsplash.com/" external>Unsplash</SimpleLink>
       <br />
-      <a href="https://openweathermap.org/" target="_blank" rel="noopener">
-        OpenWeatherMap
-      </a>
+      <SimpleLink href="https://openweathermap.org/" external
+        >OpenWeatherMap</SimpleLink
+      >
       <br />
-      <a href="https://todoist.com/" target="_blank" rel="noopener">
-        Todoist
-      </a>
+      <SimpleLink href="https://todoist.com/" external>Todoist</SimpleLink>
     </p>
     <p>Thank you for your service. Stay awesome!</p>
     <p>•</p>
@@ -22,9 +30,10 @@
 
 <script>
 import SettingsSection from "../section/SettingsSection";
+import SimpleLink from "../../SimpleLink";
 
 export default {
-  components: { SettingsSection }
+  components: { SimpleLink, SettingsSection }
 };
 </script>
 
---
+++ task-generate.js
@@ -0,0 +1,100 @@
+const fs = require("fs");
+const fsp = fs.promises;
+const path = require("path");
+const packageJson = require("./package.json");
+const archiver = require("archiver");
+const del = require("del");
+
+const DIST_PATH = "dist";
+const PACKAGES_PATH = "packages";
+
+class Task {
+  /**
+   * Prepares archive target folder
+   * @returns {Promise<void>}
+   */
+  static async init() {
+    await del(PACKAGES_PATH);
+    await fsp.mkdir(PACKAGES_PATH);
+  }
+
+  /**
+   * Updates manifest file so the browser extension can be registered
+   * @returns {Promise<void>}
+   */
+  static async updateManifest() {
+    console.log("> Manifest is being updated");
+
+    const manifestPath = `${DIST_PATH}/manifest.json`;
+    const fileContent = await fsp.readFile(manifestPath, "utf8");
+    const manifest = JSON.parse(fileContent);
+
+    manifest.version = packageJson.version;
+    manifest.description = packageJson.description;
+    manifest.homepage_url = packageJson.homepage;
+
+    await fsp.writeFile(manifestPath, JSON.stringify(manifest), "utf8");
+
+    console.log("✨ Manifest has been updated");
+  }
+
+  /**
+   * Zips dist and source files for extension upload
+   * @param {string} sourceFolder Glob patterns start folder name
+   * @param {string[]} globs Globs (https://github.com/isaacs/node-glob#options)
+   * @param {string} targetFileName Archive's file name
+   * @returns {Promise<void>}
+   */
+  static async archiveFiles(sourceFolder, globs, targetFileName) {
+    return new Promise((resolve, reject) => {
+      console.log(`> Creating ${targetFileName} archive`);
+
+      const zipPath = `${PACKAGES_PATH}/${targetFileName}.zip`;
+      const sourcePath = path.join(__dirname, sourceFolder);
+      const output = fs.createWriteStream(zipPath);
+      const archive = archiver("zip", {
+        zlib: {
+          level: 9
+        }
+      });
+
+      output.on("close", function() {
+        console.log("✨ Archive has been created");
+        resolve();
+      });
+
+      archive.on("error", function(err) {
+        reject(err);
+      });
+      archive.pipe(output);
+      for (const glob of globs) {
+        archive.glob(glob, {
+          cwd: sourcePath,
+          nodir: true,
+          dot: false
+        });
+      }
+      archive.finalize();
+    });
+  }
+
+  /**
+   * Main
+   * @returns {Promise<void>}
+   */
+  static async main() {
+    try {
+      console.log(":: Generating archives ::");
+
+      await this.init();
+      await this.updateManifest();
+      await this.archiveFiles(DIST_PATH, ["**"], "package");
+      await this.archiveFiles("", ["public/**", "src/**", "*"], "source");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+//run
+Task.main();
